bdfe4e998c611b3e0edebe115cb47bad6b33151f
Miguel Moral <miguelmoralhernandez@gmail.com>
2024-09-17 17:53:42 +0200

    evaluation file done

--- evaluation.py
+++ evaluation.py
@@ -1,10 +1,9 @@
-
 from constant import (
     OBJECT_TO_FLOAT, OBJECT_TO_INT, TIME_TO_NUMERIC_YEARS, MONTHS_TO_NUMERIC,
     MULTI_LABEL_BINARIZER_FEATURES, ORDINAL_VARIABLES, 
     CATEGORICAL_NON_ORDINAL_VARIABLES, CONTINUOUS_FEATURES, PARAMS_RF_RFE
 )
-from utils.utils import match_features, find_best_trial, get_best_model
+from utils.utils import match_features
 from data_cleanning import DataCleanning
 from imputer import ImputeMissing
 from cateorical_encoder import CategoricalEncoder
@@ -15,14 +14,8 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import os
 import joblib
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import lightgbm as lgb
-from catboost import CatBoostClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-import xgboost as xgb
 
 # Load dataframe
 df = pd.read_csv('data/train.csv')
@@ -89,48 +82,47 @@ encoder = CategoricalEncoder(
 X_train_encoded = encoder.fit_transform(X_train_imputed)
 X_test_encoded = encoder.transform(X_test_imputed)
 
-    
-'''
-For each model I have saved all the optuna hyperparameters optimization results in json files inside logs folder.
-Remember that I have been optimizing trying to get the maximum ROC AUC score and the minimum overfitting score. 
-Now it is time to select which model and with which hyperparameters will be the one we will train and evaluate. 
-To make the choice I have decided to a threshold for the overfitting score of 0.02. So, I will look to all the trials
-of all models that have an overfitting below 0.02 and I will choose the trial that has the highest ROC AUC score. 
- 
-'''
+### PREDICTIONS AND RESULTS
 
-# Use the function 'find_best_trial' to find the OPTIMAL (BEST) TRIAL
-logs_dir = 'logs' 
-best_trial_info = find_best_trial(logs_dir)
+#Load the saved model (to ensure it's properly saved)
+loaded_model = joblib.load('logs/trained_model.pkl')
 
-if best_trial_info:
-    print(f"Best model: {best_trial_info['model']}")
-    print(f"Trial number: {best_trial_info['trial_number']}")
-    print(f"ROC AUC: {best_trial_info['roc_auc']}")
-    print(f"Overfitting: {best_trial_info['overfitting']}")
-    print(f"Hyperparameters: {best_trial_info['params']}")
-else:
-    print("No trail has been found below the overfitting thershold.")
-    
-### TRAIN THE MODEL
+# Predict probabilities for each label
+y_pred_train_proba = loaded_model.predict_proba(X_train_encoded)
+y_test_pred_proba = loaded_model.predict_proba(X_test_encoded)
 
-# Get the model and its hyperparams from the 'best_trial_info'
-model = get_best_model(best_trial_info)
+# Evaluate the model's performance using ROC AUC with ovo (one-vs-one)
+roc_auc_train = roc_auc_score(y_train, y_pred_train_proba, multi_class='ovo')
+roc_auc_test = roc_auc_score(y_test, y_test_pred_proba, multi_class='ovo')
 
-# Train the model on the full training set
-model.fit(X_train_encoded, y_train)
+print(f"Train ROC AUC Score: {roc_auc_train}")
+print(f"Test ROC AUC Score: {roc_auc_test}")
 
-### SAVE THE MODEL TO A PICKLE FILE IN THE 'logs' FOLDER
+# Generate class predictions from the model
+y_train_pred = loaded_model.predict(X_train_encoded)
+y_test_pred = loaded_model.predict(X_test_encoded)
 
-os.makedirs(logs_dir, exist_ok=True)
-model_filename = os.path.join(logs_dir, 'trained_model.pkl')
-joblib.dump(model, model_filename)
+# Create the confusion matrix for the test set
+conf_matrix = confusion_matrix(y_test, y_test_pred)
 
+# Define the labels
+labels = ['Poor', 'Standard', 'Good']
 
+# Create the heatmap
+plt.figure(figsize=(10, 7))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
 
+# Customize the plot
+plt.title('Confusion Matrix')
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
 
+# Ensure the 'logs' folder exists
+os.makedirs('logs', exist_ok=True)
 
+# Save and plot
+plt.savefig('logs/confusion_matrix.png', dpi=300, bbox_inches='tight')
+plt.show()
 
-
-
-
+# Calculate and print classification report
+print(classification_report(y_test, y_test_pred, target_names=['Poor', 'Standard', 'Good']))
